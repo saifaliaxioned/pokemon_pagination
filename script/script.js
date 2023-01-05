af77cd7bfde2754a388ab87a79707c467cbac4f8
saifaliaxioned <saifalis@axioned.com>
2023-01-05 19:39:44 +0530

    axios added

--- script/script.js
+++ script/script.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const navLink = document.querySelectorAll(".nav-items a"),
   locations = location.pathname,
   pokemonItems = document.querySelector('.pokemon-items'),
