32b97495008d8b1b5aba8b67cb284abca6b871b8
saifaliaxioned <saifalis@axioned.com>
2023-01-09 19:44:43 +0530

    pagination added

--- src/_data/detail.js
+++ src/_data/detail.js
@@ -3,15 +3,20 @@ const axios = require('axios');
 module.exports = async function() {
   try {
     let arr= [];
-    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=50`);
+    // const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=50`);
     for (let i = 0; i < 50; i++) {
-      let pokemon = await axios.get(`https://pokeapi.co/api/v2/pokemon/${i+1}`);
+      let pokemon = await axios.get(`https://pokeapi.co/api/v2/pokemon/${i+1}`); 
       arr.push(pokemon);
-      console.log(arr);
-      return arr;
+      // return arr;
     }
-    // console.log(response.data);
-    return response.data;
+    // console.log(arr[0]);
+    return arr;
+    // console.log(arr);
+    // arr.forEach(obj =>{
+    //   // console.log(obj.data.name);
+    //   return obj;
+    // })
+    // return response.data;
 
   } catch (error) {
     console.error(error);
