3434655252816d92da195e77f5afc7e17970fddb
saifaliaxioned <saifalis@axioned.com>
2023-01-16 19:11:04 +0530

    detail page updated

--- dist/script/script.js
+++ dist/script/script.js
@@ -2,9 +2,11 @@ const navLink = document.querySelectorAll(".nav-items a"),
  pagelink = document.querySelectorAll(".page-list a"),
   locations = location.pathname;
 
+  console.log(locations.split('/')[1]);
 navLink.forEach(el => {
   const link = el.getAttribute("href");
-  if (locations === link) {
+  console.log(link.split('/')[1]);
+  if (locations.split('/')[1] === link.split('/')[1]) {
     const active = document.querySelector(".active-link");
     if (active) {
       active.classList.remove("active-link")
